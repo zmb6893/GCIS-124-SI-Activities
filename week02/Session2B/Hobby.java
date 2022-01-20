291c25cca4b52f3e531b85ccc85713b2bdcf449a
zmb6893 <zmb6893@rit.edu>
2022-01-20 13:53:00 -0500

    Activity for Session2B created

---
+++ week02/Session2B/Hobby.java
@@ -0,0 +1,53 @@
+package week02.Session2B;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Session 2B: Introduction to java classes.
+ * @author: Zoe Bingham
+ */
+
+public class Hobby {
+    // CONSTANTS
+    private String FORMAT =  "%s:\t %s";
+
+    // Add the attributes for a hobby name and a hobby description
+
+    /**
+     * Make a hobby with the given description and name.
+     * @param name
+     * @param description
+     */
+    
+
+    // Add the appropriate getters and setters
+    
+
+    // Add the appropriate action methods to this class.
+    /**
+     * Print out the name of the hobby followed by the description of the hobby.
+     */
+    
+
+    // BONUS ACTIVITY
+    /**
+     * Make a method that returns an array of hobbies from a given file. First line 
+     * is a hobby and the next line is the description
+     * @param filename
+     * @return
+     */
+    public static Hobby[] readFileForHobbies(String filename){
+        Hobby[] hobbies = new Hobby[4];
+
+        return hobbies;
+    }
+
+    public static void main(String[] args){
+        // Make an instance of a hobby
+
+        // Call the doHobby with the hobby that has just been created
+
+    }
+}
---
+++ week02/Session2B/Person.java
@@ -0,0 +1,47 @@
+package week02.Session2B;
+
+/**
+ * Session 2B: Introduction to java classes.
+ * @author: Zoe Bingham
+ */
+
+public class Person {
+    // Add the attributes name, age, hobbies
+
+    /**
+     * Make a constructor for the person class that takes arguments for a name and age
+     * @param name
+     * @param age
+     */
+
+    // Getters
+
+
+    // Setters
+    // (Names should not be alterable and age should not decrease)
+    /**
+     * Increment the age by one year,
+     */
+
+
+    /**
+     * Change the name to the specified name
+     * @param name
+     */
+
+
+    /**
+     * Add the provided hobby to the person's list of hobbies.
+     * @param hobby
+     */
+
+
+    public static void main(String[] args){
+        // Make an instance of a Person class with your name.
+
+        // Make a few instances of hobbies and add those to the list of hobbies
+
+        // Do all the hobbies that that person has using the method you made
+
+    }
+}
