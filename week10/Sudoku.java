10bccbd26aad387043bc20eb740e9fa2333bc6f2
zmb6893 <zmb6893@rit.edu>
2022-03-20 14:26:28 -0400

    Week 10 sudoku backtracker

---
+++ week10/Sudoku.java
@@ -0,0 +1,15 @@
+/**
+ * Sudoku backtracker
+ * @author 
+ */
+
+// What should we do to make sure that we can backtrack?
+public class Sudoku {
+    // A sudoku board is a 2D array of ints
+
+    // This will be to print out a board
+    @Override
+    public String toString() {
+        return null;
+    }
+}
